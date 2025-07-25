afc8e916d339ea37a80efd0fc87ae9c42b5f57ed
kh3ra <adityakh3ra@gmail.com>
2025-07-25 12:49:51 +0530

    new tests + test fixes + minor bug fixes

--- test/framework/src/main/java/org/opensearch/index/shard/IndexShardTestCase.java
+++ test/framework/src/main/java/org/opensearch/index/shard/IndexShardTestCase.java
@@ -178,7 +178,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
